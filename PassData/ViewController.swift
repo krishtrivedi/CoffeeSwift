dbe91fe923d297c1a8b164911b65625bfb679583
Krishna <krishna@krishnas-macbook-pro.local>
2021-12-14 18:41:44 -0700

    Initial Commit

---
+++ PassData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassData
+//
+//  Created by Krishna on 12/14/21.
+//  Copyright © 2021 Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
